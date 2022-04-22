3d210a2824e913fd765eeb8eabd798bae4b8738f
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-04-22 15:12:24 -0700

    model and route comment testing

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,22 +1,46 @@
 //IMPORT THE PARTS NEEDED FROM THE MONGOOSE LIB
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            //GETTER METHOD
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        //MONGOOSE RETURNS THIS VIRTUAL SO THE ID IS NA
+        id: false
+    }
+);
+
+//ADD VIRTUAL TO COUNT THE NUMBER OF COMMENTS ON RETRIEVAL
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
 });
 
 //MAKE THE MODEL
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+//IMPORT ALL ROUTES IN THE API FOLDER
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
+
+//ADD PREFIX OF COMMENTS TO ROUTES IN COMMENT-ROUTES.JS
+router.use('/comments', commentRoutes);
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
