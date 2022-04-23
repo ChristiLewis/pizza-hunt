272260312e4b0216f4b8df199b9bcdbca19eea2d
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-04-22 17:35:12 -0700

    comment model and routes passed Insomnia testing

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -6,6 +6,15 @@ const pizzaController = {
     //GET ALL
     getAllPizza(req, res) {
         Pizza.find({})
+            //ADD .POPULATE METHOD TO SEE THE ACTUAL COMMENTS ASSOCIATED WITH THE PIZZA INSTEAD OF ONLY THE OBJECTID
+            .populate({
+                path: 'comments',
+                select: '-_v'
+            })
+            //ADD .SELECT() METHOD TO EDIT OUT THE RETURN OF THE _VFIELD FOR THE MODEL TOO
+            .select('-_v')
+            //.SORT() METHOD IN DESCENDING ORDER
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -16,6 +25,13 @@ const pizzaController = {
     //GET ONE BY ID
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+            //ADD .POPULATE METHOD TO SEE THE ACTUAL COMMENTS ASSOCIATED WITH THE PIZZA INSTEAD OF ONLY THE OBJECTID
+            .populate({
+                path: 'comments',
+                select: '-_v'
+            })
+            //ADD .SELECT() METHOD TO EDIT OUT THE RETURN OF THE _VFIELD FOR THE MODEL TOO
+            .select('-_v')
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No Pizza found with this id!' });
