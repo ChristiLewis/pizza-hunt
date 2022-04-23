347f3ca7c6d587afffe546344797d0058e529078
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-04-23 12:26:04 -0700

    replies tested in browser

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -148,8 +148,29 @@ function handleNewReplySubmit(event) {
   }
 
   const formData = { writtenBy, replyBody };
+  //FETCH FUNCTIONALITY CONNECTS THE BACKEND TO THE FRONTEND!
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
-
 $backBtn.addEventListener('click', function () {
   window.history.back();
 });
